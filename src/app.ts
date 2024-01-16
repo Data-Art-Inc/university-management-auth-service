e91f2200213236427f4641162f7e779231d81446
Mahfuj Ahsan <mahfujur.rahman1@outlook.com>
2024-01-16 11:18:04 +0600

    [DONE] Optimized and Organized Code Base

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from 'cors'
 import express, { Application } from 'express'
 const app: Application = express()
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/user/user.route'
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import routes from './app/routes'
 
 app.use(cors())
 
@@ -12,8 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Application Routes
-app.use('/api/v1/users', UserRoutes)
-app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1', routes);
 
 // // Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
